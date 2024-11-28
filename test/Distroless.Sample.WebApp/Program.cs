3d5010a3f419f840394759201e2c7262e1ffa430
Martossy Alex <github@mail.martossy.hu>
2024-11-28 12:49:00 +0100

    Add logging to sample app

--- test/Distroless.Sample.WebApp/Program.cs
+++ test/Distroless.Sample.WebApp/Program.cs
@@ -1,38 +1,59 @@
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateSlimBuilder(args);
-
-builder.Services.ConfigureHttpJsonOptions(options =>
+WebApplication app;
+try
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
+    var builder = WebApplication.CreateSlimBuilder(args);
 
-var app = builder.Build();
+    builder.Logging.ClearProviders().AddConsole();
 
-var sampleTodos = new Todo[]
+    builder.Services.ConfigureHttpJsonOptions(options =>
+    {
+        options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    });
+
+    app = builder.Build();
+}
+catch (Exception e)
 {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
+    Console.WriteLine("Error building application:");
+    Console.WriteLine(e);
+    throw;
+}
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id:int}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo ? Results.Ok(todo) : Results.NotFound());
-
-app.MapGet("/healthz/{desired:int?}", (int? desired) => desired switch
+var logger = app.Logger;
+try
 {
-    null => Results.Ok(),
-    0 => Results.Ok(new HealthStatus("Healthy")),
-    1 => Results.InternalServerError(new HealthStatus("Unhealthy")),
-    2 => Results.InternalServerError(new HealthStatus("Degraded")),
-    _ => Results.BadRequest(),
-});
+    var sampleTodos = new Todo[]
+    {
+        new(1, "Walk the dog"),
+        new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+        new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+        new(4, "Clean the bathroom"),
+        new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+    };
 
-app.Run();
+    var todosApi = app.MapGroup("/todos");
+    todosApi.MapGet("/", () => sampleTodos);
+    todosApi.MapGet("/{id:int}", (int id) =>
+        sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo ? Results.Ok(todo) : Results.NotFound());
+
+    app.MapGet("/healthz/{desired:int?}", (int? desired) => desired switch
+    {
+        null => Results.Ok(),
+        0 => Results.Ok(new HealthStatus("Healthy")),
+        1 => Results.InternalServerError(new HealthStatus("Unhealthy")),
+        2 => Results.InternalServerError(new HealthStatus("Degraded")),
+        _ => Results.BadRequest(),
+    });
+
+    app.Run();
+}
+catch (Exception e)
+{
+    logger.LogCritical(e, "Application has crashed");
+    throw;
+}
 
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
