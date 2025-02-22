d8345ca3a39b66857e8dc82cfbcc3edb0b0ebc22
Alex Martossy <github@mail.martossy.hu>
2025-02-22 17:17:34 +0100

    Add .net 10 images (#211)
    * Add .net 10 images
    
    * Try fix tests
    
    * Add more test images
    
    * Try nuget sources
    
    * Re add AOT deps to sample app
    
    * Add changeset
    
    * Change nuget source
    
    * Disable verbose trx reporter
    
    * Fix dotnet TFM parsing logic in UrlsTest
    
    * Alter regex
    
    * Add docs

---
+++ test/Distroless.HealthChecks.Test/HealthyTests/Net10/RuntimeDeps10AotContainerTest.cs
@@ -0,0 +1,21 @@
+namespace Distroless.HealthChecks.Test.HealthyTests.Net10;
+
+public class RuntimeDeps10AotContainerTest(ITestOutputHelper output, ITestContextAccessor testContext)
+    : HealthyContainerTest<RuntimeDeps10AotData>(output, testContext);
+
+public abstract class RuntimeDeps10AotData : ITestData
+{
+    public static TheoryData<string, string, string, string> GetTheoryData()
+    {
+        var images =
+            Utils.GetImageTagsFromDockerfile(
+                "test/Distroless.Sample.WebApp/healthyContainerTest.runtime-deps10-aot.bases.Dockerfile");
+        var data = new TheoryData<string, string, string, string>();
+        foreach (var image in images)
+        {
+            data.Add(image.Image, image.Tag, "10.0", "test/Distroless.Sample.WebApp/Dockerfile");
+        }
+
+        return data;
+    }
+}
